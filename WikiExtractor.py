5328bd8394a3267ff18f79908c8d37d62921fe6a
Katsuya Shimabukuro <katsu.generation.888@gmail.com>
2019-04-17 14:41:24 +0900

    Fix abstract_only option

--- WikiExtractor.py
+++ WikiExtractor.py
@@ -198,6 +198,9 @@ options = SimpleNamespace(
         'ref', 'references', 'img', 'imagemap', 'source', 'small',
         'sub', 'sup', 'indicator'
     ],
+
+    # Output only abstract 
+    abstract_only = False,
 )
 
 ##
@@ -3161,6 +3164,7 @@ def main():
     options.expand_templates = args.no_templates
     options.filter_disambig_pages = args.filter_disambig_pages
     options.keep_tables = args.keep_tables
+    options.abstract_only = abstract_only
 
     try:
         power = 'kmg'.find(args.bytes[-1].lower()) + 1
